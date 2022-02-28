6ff42255ff7f009626eefd9b17d65f8ead0b196c
Aleksei Baev <kruzenshteyn@yandex.ru>
2022-02-28 16:19:19 +0300

    404_adaptive

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,13 +1,19 @@
 //import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import './NotFoundPage.css';
 
+
+
 function NotFoundPage(props) {
+
+  const history = useHistory();
 
   return (
     <div className="notFoundPage">
       <h1 className='notFoundPage__title'>404</h1>
       <p className='notFoundPage__text'>Страница не найдена</p>      
-      <p  className='notFoundPage__link'>Назад //p</p>
+      <button type='button'  className='notFoundPage__link' onClick={history.goBack}>Назад</button>
     </div>
   );
 }
